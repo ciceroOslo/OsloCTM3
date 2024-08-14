ab8ab6779399da0d0bbc3b5f1a1458a2356db085
ragnhibs <r.b.skeie@cicero.oslo.no>
2024-08-14 10:32:33 +0200

    removed H2 sensitivit test emissions

--- src/OSLO/emissions_aircraft.f90
+++ src/OSLO/emissions_aircraft.f90
@@ -40,10 +40,10 @@ module emissions_aircraft
   integer, parameter :: maxlevels = 66
 
   !// Number of species emitted (used to be only 1: NO)
-  integer, parameter :: EPAR_AC = 2 !RBS24 !RBS19
+  integer, parameter :: EPAR_AC = 1 !RBS24 !RBS19
   !// List of all these component species
   character(len=10), dimension(EPAR_AC), parameter :: &
-       ECOMP_NAMES = (/'NO','H2'/)!(/'CO', 'C2H4', 'C2H6','C3H6', 'C6H14',&
+       ECOMP_NAMES = (/'NO'/)!(/'CO', 'C2H4', 'C2H6','C3H6', 'C6H14',&
        !'C6HXR', 'C6HXR_SOA', &
        !'CH2O','CH3CHO','NO','C3H8','ACETONE','SO2',&
        !'Tolmatic','Benzene','omFF1fob','omFF1fil','bcFF1fob','bcFF1fil'/)
