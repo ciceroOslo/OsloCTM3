47ad1970ae8eabaa058ceca3e9eda0b664336763
ragnhibs <r.b.skeie@cicero.oslo.no>
2025-03-03 14:22:32 +0100

    corrected variable name for error message

--- src/OSLO/diagnostics_general.f90
+++ src/OSLO/diagnostics_general.f90
@@ -3798,8 +3798,8 @@ contains
        !// Only put out if included
        if (trsp_idx(compsPL(N)) .le. 0) cycle
 
-       varname = trim(TNAME(N))//'_LOSS'
-
+       !//varname = trim(TNAME(N))//'_LOSS'
+       varname = trim(TNAME(trsp_idx(compsPL(N))))//'_LOSS'
        !// Lost mass in chemistry
        do L = 1, LPAR
           do J = 1, JPAR
@@ -3815,6 +3815,7 @@ contains
        if (status .ne. nf90_noerr) call handle_error(status, &
             f90file//':'//subr//': putting '//trim(varname))
 
+       varname = trim(TNAME(trsp_idx(compsPL(N))))//'_PROD'
        !// Produced mass in chemistry
        do L = 1, LPAR
           do J = 1, JPAR
