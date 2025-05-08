c355d48c95fa68f821a87ab53b702568ef0fffc9
ragnhibs <r.b.skeie@cicero.oslo.no>
2025-05-08 12:06:04 +0200

    Fixed delta_time in diagnostics

--- src/OSLO/diagnostics_general.f90
+++ src/OSLO/diagnostics_general.f90
@@ -101,9 +101,8 @@ module diagnostics_general
   real(r8), dimension(LPAR,IPAR,JPAR) :: OxCHEMLOSSMASS
   real(r8), dimension(LPAR,IPAR,JPAR) :: OxCHEMPRODMASS
   !// Only save prod and loss for selected components
-  integer,parameter :: ncPL = 13 !RBS 4-5 -> 6
-  !//integer,dimension(ncPL), parameter :: compsPL = (/13,20,46,113,114,6,16,52/) !Add CO, MHP
-  integer,dimension(ncPL), parameter :: compsPL = (/5,6,7,8,13,16,20,35,46,50,52,113,114/) !Add for VOCMIP
+  integer,parameter :: ncPL = 23 !RBS 4-5 -> 6
+  integer,dimension(ncPL), parameter :: compsPL = (/5,6,7,8,9,13,16,20,35,46,48, 50,52,77, 78, 80, 81, 83, 84, 85,86,113,114/) !Add for VOCMIP
 
   !// ----------------------------------------------------------------------
   character(len=*), parameter, private :: f90file='diagnostics_general.f90'
@@ -3048,7 +3047,7 @@ contains
          f90file//':'//subr//': putting ihyb')
 
     !// delta-time in seconds
-    DTDIAG = real(NDAY + 1 - NDAY0, r8) * 84600._r8
+    DTDIAG = real(NDAY + 1 - NDAY0, r8) * 86400._r8
     status = nf90_put_var(ncid,time_id,DTDIAG)
     if (status .ne. nf90_noerr) call handle_error(status, &
          f90file//':'//subr//': putting NRAVG')
@@ -3772,7 +3771,7 @@ contains
          f90file//':'//subr//': putting ihyb')
 
     !// delta-time in seconds
-    DTDIAG = real(NDAY + 1 - NDAY0, r8) * 84600._r8
+    DTDIAG = real(NDAY + 1 - NDAY0, r8) * 86400._r8
     status = nf90_put_var(ncid,time_id,DTDIAG)
     if (status .ne. nf90_noerr) call handle_error(status, &
          f90file//':'//subr//': putting NRAVG')
