9971d5dbe63d07ace1cea292a6fc8eca6d8d977d
Marianne Lund <mariantl@login-5.saga>
2025-10-16 19:19:10 +0200

    Fixed bugs in chemistry output, added more 3hrly and 1hrly output variables and added routing for reading HTAP3 aviation emissions

--- src/OSLO/diagnostics_general.f90
+++ src/OSLO/diagnostics_general.f90
@@ -101,8 +101,8 @@ module diagnostics_general
   real(r8), dimension(LPAR,IPAR,JPAR) :: OxCHEMLOSSMASS
   real(r8), dimension(LPAR,IPAR,JPAR) :: OxCHEMPRODMASS
   !// Only save prod and loss for selected components
-  integer,parameter :: ncPL = 4
-  integer,dimension(ncPL), parameter :: compsPL = (/13,46,113,114/)
+  integer,parameter :: ncPL = 7
+  integer,dimension(ncPL), parameter :: compsPL = (/1,6,46,40,72,73,107/)
 
   !// ----------------------------------------------------------------------
   character(len=*), parameter, private :: f90file='diagnostics_general.f90'
@@ -115,7 +115,8 @@ module diagnostics_general
        write_snapshot, &
        ch4n2o_burden, &
        tnd_emis2file, &
-       nchemdiag, save_chemPL, save_chemOxPL, chembud_output, init_chembud
+       nchemdiag, save_chemPL, save_chemOxPL, chembud_output, init_chembud, &
+       chembud_output_nc !MTL added 
   !// ----------------------------------------------------------------------
 
 contains
@@ -3509,27 +3510,45 @@ contains
     status = nf90_def_dim(ncid,"lev",LPAR,lev_dim_id)
     if (status .ne. nf90_noerr) call handle_error(status, &
          f90file//':'//subr//': define lev dim')
-    status = nf90_def_dim(ncid,"nchemdiag",nchemdiag_id,nchemdiag_dim_id)
+
+       
+   !//MTL define spatial dimensions of ilat/ilon/ilev
+    status = nf90_def_dim(ncid,"ilat",JPAR+1,ilat_dim_id)
+    if (status .ne. nf90_noerr) call handle_error(status, &
+         f90file//':'//subr//': define ilat dim')
+    status = nf90_def_dim(ncid,"ilon",IPAR+1,ilon_dim_id)
+    if (status .ne. nf90_noerr) call handle_error(status, &
+         f90file//':'//subr//': define ilon dim')
+    status = nf90_def_dim(ncid,"ilev",LPAR+1,ilev_dim_id)
+    if (status .ne. nf90_noerr) call handle_error(status, &
+         f90file//':'//subr//': define ilev dim')
+    !// Define ilon/ilat/ilev - MTL: wrong definition 
+    !status = nf90_def_var(ncid,"ilon",nf90_double,ilon_dim_id,ilon_id)
+    !if (status .ne. nf90_noerr) call handle_error(status, &
+    !     f90file//':'//subr//': define ilon variable')
+    !status = nf90_def_var(ncid,"ilat",nf90_double,ilat_dim_id,ilat_id)
+    !if (status .ne. nf90_noerr) call handle_error(status, &
+    !     f90file//':'//subr//': define ilat variable')
+    !status = nf90_def_var(ncid,"ilev",nf90_double,ilev_dim_id,ilev_id)
+    !if (status .ne. nf90_noerr) call handle_error(status, &
+    !     f90file//':'//subr//': define ilev variable')
+    !//MTL
+
+    !//MTL
+    status = nf90_def_dim(ncid,"nchemdiag",nchemdiag,nchemdiag_dim_id)
     if (status .ne. nf90_noerr) call handle_error(status, &
          f90file//':'//subr//': define nchemdiag dim')
-
-
-    !// Define ilon/ilat/ilev
-    status = nf90_def_var(ncid,"ilon",nf90_double,ilon_dim_id,ilon_id)
-    if (status .ne. nf90_noerr) call handle_error(status, &
-         f90file//':'//subr//': define ilon variable')
-    status = nf90_def_var(ncid,"ilat",nf90_double,ilat_dim_id,ilat_id)
-    if (status .ne. nf90_noerr) call handle_error(status, &
-         f90file//':'//subr//': define ilat variable')
-    status = nf90_def_var(ncid,"ilev",nf90_double,ilev_dim_id,ilev_id)
-    if (status .ne. nf90_noerr) call handle_error(status, &
-         f90file//':'//subr//': define ilev variable')
-
+    !status = nf90_def_dim(ncid,"nchemdiag",nchemdiag_id,nchemdiag_dim_id)
+    !if (status .ne. nf90_noerr) call handle_error(status, &
+    !     f90file//':'//subr//': define nchemdiag dim')
+    !//MTL
+    
     !// Define size of date stamps
     status = nf90_def_dim(ncid,"date_size",size(start_time),date_size_dim_id)
     if (status .ne. nf90_noerr) call handle_error(status, &
          f90file//':'//subr//': define date_size dim')
-
+    
+    
     !//---------------------------------------------------------------------
 
     !// Define the lon/lat/lev
@@ -3550,11 +3569,17 @@ contains
     status = nf90_def_var(ncid,"ilat",nf90_double,ilat_dim_id,ilat_id)
     if (status .ne. nf90_noerr) call handle_error(status, &
          f90file//':'//subr//': define ilat variable')
-    !// Define time
-    status = nf90_def_var(ncid,"time",nf90_double,time_dim_id,time_id)
+    status = nf90_def_var(ncid,"ilev",nf90_double,ilev_dim_id,ilev_id)
     if (status .ne. nf90_noerr) call handle_error(status, &
-         f90file//':'//subr//': define time variable')
-
+         f90file//':'//subr//': define ilev variable')
+    
+    !//MTL
+    !// Define time
+    !status = nf90_def_var(ncid,"time",nf90_double,time_dim_id,time_id)
+    !if (status .ne. nf90_noerr) call handle_error(status, &
+    !     f90file//':'//subr//': define time variable')
+    !//MTL
+    
     !// Putting attributes to lon/lat/lev variables
     status = nf90_put_att(ncid,lon_id,'units','degree_east')
     if (status .ne. nf90_noerr) call handle_error(status, &
@@ -3769,12 +3794,15 @@ contains
 
     !// For each tracer put out loss and prod
     do N = 1, ncPL
-
+       TRID = compsPL(N)   !MTL 
+       TRNR = trsp_idx(TRID) !MTL
+       
        !// Only put out if included
        if (trsp_idx(compsPL(N)) .le. 0) cycle
 
-       varname = trim(TNAME(N))//'_LOSS'
-
+       !varname = trim(TNAME(N))//'_LOSS'
+       varname = trim(TNAME(trsp_idx(compsPL(N))))//'_LOSS' !MTL
+       
        !// Lost mass in chemistry
        do L = 1, LPAR
           do J = 1, JPAR
@@ -3790,6 +3818,7 @@ contains
        if (status .ne. nf90_noerr) call handle_error(status, &
             f90file//':'//subr//': putting '//trim(varname))
 
+       varname = trim(TNAME(trsp_idx(compsPL(N))))//'_PROD'  !MTL
        !// Produced mass in chemistry
        do L = 1, LPAR
           do J = 1, JPAR
