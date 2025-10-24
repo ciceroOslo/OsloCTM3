966fa3955644677363d28e5691ee7cc2c769a529
zosiast <zosia.staniaszek@cicero.oslo.no>
2025-10-24 11:29:58 +0200

    Diagnostics for ozone, OH, and SO4 for HTAP3

--- src/OSLO/pchemc_ij.f90
+++ src/OSLO/pchemc_ij.f90
@@ -1224,12 +1224,21 @@ contains
                  + k_oh_ho2 * HO2_OLD  !OH + HO2 -> H2O + O2
             !// Set new OH
             OH_NEW = PROD / LOSS
+<<<<<<< HEAD
 
             !ZS
             OxCHEMPROD(L) = OxCHEMPROD(L) + PROD * DTCH
             OxCHEMLOSS(L) = OxCHEMLOSS(L) + LOSS * M_OH * DTCH 
             !ZS
 
+=======
+            
+            !ZS
+            CHEMLOSS(1,40,L) = CHEMLOSS(1,40,L) + LOSS * DTCH
+            CHEMPROD(1,40,L) = CHEMPROD(1,40,L) + PROD * M_OH * DTCH
+            
+            !ZS
+>>>>>>> 9d6df0b (Diagnostics for ozone, OH, and SO4 for HTAP3)
 
             !//..HO2--------------------------------------------------------
             PROD = &
@@ -2247,6 +2256,7 @@ contains
 
         !ZS ozone diags
         CHEMLOSS(2,1,L) = CHEMLOSS(2,1,L) + VDEP_L(1) * M_O3 * DTCH
+<<<<<<< HEAD
         CHEMLOSS(3,1,L) = CHEMLOSS(3,1,L) + k_od_h2o * M_H2O * M_O1D * DTCH
         CHEMLOSS(4,1,L) = CHEMLOSS(4,1,L) + ((k_o3_ho2 * M_HO2 &
                 + k_o3_oh * M_OH &
@@ -2257,6 +2267,18 @@ contains
                 - VDEP_L(43) &
                 - k_op_no_m * M_O3P) * M_NO * DTCH
         CHEMPROD(2,1,L) = CHEMPROD(2,1,L) + 2._r8 * DO2 * M_O2 * DTCH
+=======
+        CHEMLOSS(3,1,L) = CHEMLOSS(3,1,L) + k_od_h2o * M_H2O * M_O1D
+        CHEMLOSS(4,1,L) = CHEMLOSS(4,1,L) + (k_o3_ho2 * M_HO2 &
+                + k_o3_oh * M_OH &
+                + k_o3_c3h6 * M_C3H6 &
+                + k_o3_c2h4 * M_C2H4) * M_O3 &
+                + k_od_h2o * M_H2O * M_O1D
+        CHEMPROD(1,1,L) = CHEMPROD(3,1,L) + (LOSS_2 &
+                - VDEP_L(43) &
+                - k_op_no_m * M_O3P) * M_NO 
+        CHEMPROD(2,1,L) = CHEMPROD(4,1,L) + 2._r8 * DO2 * M_O2
+>>>>>>> 9d6df0b (Diagnostics for ozone, OH, and SO4 for HTAP3)
         !ZS
 
         !//..H2O2------------------------------------------------------------
@@ -2868,7 +2890,7 @@ contains
                    + POLLX(73)
               LOSS = VDEP_L(73)
 
-              !ZS 3=gaseous, 4=aqueous
+              !ZS 2=gaseous, 3=aqueous
               CHEMPROD(1,73,L) = CHEMPROD(1,73,L) + PROD * DTCH
               CHEMPROD(2,73,L) = CHEMPROD(2,73,L) + CTOT4072 * M_OH * M_SO2 * DTCH
               CHEMPROD(3,73,L) = CHEMPROD(3,73,L) + (CAQ1572 * M_H2O2 &            
