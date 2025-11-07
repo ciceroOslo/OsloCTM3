829c29c4ae90447b07c7f8f8f1957796080ea237
zosiast <zosia.staniaszek@cicero.oslo.no>
2025-11-07 11:35:41 +0100

    updated to make chemPL diags work, including fix for OH in OxCHEMLOSS/PROD

--- src/OSLO/pchemc_ij.f90
+++ src/OSLO/pchemc_ij.f90
@@ -1225,6 +1225,10 @@ contains
             !// Set new OH
             OH_NEW = PROD / LOSS
 
+            !ZS
+            OxCHEMPROD(L) = OxCHEMPROD(L) + PROD * DTCH
+            OxCHEMLOSS(L) = OxCHEMLOSS(L) + LOSS * M_OH * DTCH 
+            !ZS
 
 
             !//..HO2--------------------------------------------------------
@@ -2236,11 +2240,24 @@ contains
              + PROD_NO
 
         !// Sum up prod and loss (both are per second here)
-        OxCHEMPROD(L) = OxCHEMPROD(L) + PROD*DTCH
-        OxCHEMLOSS(L) = OxCHEMLOSS(L) + LOSS*DTCH
+        !ZS OxCHEMPROD(L) = OxCHEMPROD(L) + PROD*DTCH
+        !ZS OxCHEMLOSS(L) = OxCHEMLOSS(L) + LOSS*DTCH
 
         M_O3NO = M_O3NO + (PROD - LOSS) * DTCH
 
+        !ZS ozone diags
+        CHEMLOSS(2,1,L) = CHEMLOSS(2,1,L) + VDEP_L(1) * M_O3 * DTCH
+        CHEMLOSS(3,1,L) = CHEMLOSS(3,1,L) + k_od_h2o * M_H2O * M_O1D * DTCH
+        CHEMLOSS(4,1,L) = CHEMLOSS(4,1,L) + ((k_o3_ho2 * M_HO2 &
+                + k_o3_oh * M_OH &
+                + k_o3_c3h6 * M_C3H6 &
+                + k_o3_c2h4 * M_C2H4) * M_O3 &
+                + k_od_h2o * M_H2O * M_O1D ) * DTCH
+        CHEMPROD(1,1,L) = CHEMPROD(1,1,L) + (LOSS_2 &
+                - VDEP_L(43) &
+                - k_op_no_m * M_O3P) * M_NO * DTCH
+        CHEMPROD(2,1,L) = CHEMPROD(2,1,L) + 2._r8 * DO2 * M_O2 * DTCH
+        !ZS
 
         !//..H2O2------------------------------------------------------------
         PROD = &
@@ -2850,6 +2867,16 @@ contains
                    ) * M_SO2 &
                    + POLLX(73)
               LOSS = VDEP_L(73)
+
+              !ZS 3=gaseous, 4=aqueous
+              CHEMPROD(1,73,L) = CHEMPROD(1,73,L) + PROD * DTCH
+              CHEMPROD(2,73,L) = CHEMPROD(2,73,L) + CTOT4072 * M_OH * M_SO2 * DTCH
+              CHEMPROD(3,73,L) = CHEMPROD(3,73,L) + (CAQ1572 * M_H2O2 &            
+                      + CAQ1772 * M_HO2NO2 &
+                      + CAQ0172 * M_O3 &
+                      + CCATSO2 &
+                    ) * M_SO2 * DTCH
+              !ZS
 
               call QSSA(47,'SO4',DTCH,QLIN,ST,PROD,LOSS,ZC(73,L))
 
